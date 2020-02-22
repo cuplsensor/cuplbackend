8703089a5e3ffd78b4e0576622bc57e620a0fdd7
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-02-22 14:26:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -24,9 +24,7 @@ app = DispatcherMiddleware(simple, {
     })
 app = DebuggedApplication(app, evalex=False)
 app.debug = True
-
-if __name__ == "__main__":
-    app.run()
-    #wsb_host = os.getenv("WSB_HOST", test_defaults.WSB_HOST)
-    #wsb_port = os.getenv("WSB_PORT", test_defaults.WSB_PORT)
-    #run_simple(wsb_host, wsb_port, app, use_reloader=True, use_debugger=True)
+app.run()
+#wsb_host = os.getenv("WSB_HOST", test_defaults.WSB_HOST)
+#wsb_port = os.getenv("WSB_PORT", test_defaults.WSB_PORT)
+#run_simple(wsb_host, wsb_port, app, use_reloader=True, use_debugger=True)
