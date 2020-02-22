08920c1aa254eb1451600d06bf16ce6d579de4d0
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-02-22 14:05:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ app = DebuggedApplication(app, evalex=False)
 
 if __name__ == "__main__":
     app.debug = True
-    wsb_host = os.getenv("WSB_HOST", test_defaults.WSB_HOST)
-    wsb_port = os.getenv("WSB_PORT", test_defaults.WSB_PORT)
-    run_simple(wsb_host, wsb_port, app, use_reloader=True, use_debugger=True)
+    app.run()
+    #wsb_host = os.getenv("WSB_HOST", test_defaults.WSB_HOST)
+    #wsb_port = os.getenv("WSB_PORT", test_defaults.WSB_PORT)
+    #run_simple(wsb_host, wsb_port, app, use_reloader=True, use_debugger=True)
