912a236a43f8a2cefaaf56cea80ed6b17a4bda5f
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-03-03 09:46:07 +0000

    Update __init__.py

--- apiwrapper/admin/__init__.py
+++ apiwrapper/admin/__init__.py
@@ -4,7 +4,7 @@ from .. import ApiWrapper
 
 class AdminApiWrapper(ApiWrapper):
     def get_admin_token(self):
-        tokenurl = "{adminapiurl}/token".format(adminapiurl=self.apiurl)
+        tokenurl = "{apiurl}/token".format(apiurl=self.apiurl)
         tokenpayload = {'client_id': self.adminapi_client_id, 'client_secret': self.adminapi_client_secret}
         jsonheader = {'content-type': 'application/json'}
         r = requests.post(tokenurl, json=tokenpayload, headers=jsonheader)
@@ -15,6 +15,6 @@ class AdminApiWrapper(ApiWrapper):
         super().__init__(baseurl)
         self.adminapi_client_id = adminapi_client_id
         self.adminapi_client_secret = adminapi_client_secret
-        self.apiurl = "{baseurl}/api/admin/v1".format(baseurl=self.baseurl)
+        self.apiurl = "{baseurl}/api/admin/v1".format(baseurl=baseurl)
         tokenstr = self.get_admin_token()
         self.headers = self.auth_header(tokenstr)
