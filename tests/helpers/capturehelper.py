9d4e4c2022209b99dd14561e3ead280b6f0097c7
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-03-03 09:29:34 +0000

    Update capturehelper.py

--- tests/helpers/capturehelper.py
+++ tests/helpers/capturehelper.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 import pytz
-from wsbackend.apiwrapper.admin.capture import CaptureWrapper
+from ...apiwrapper.admin.capture import CaptureWrapper
 
 tz = pytz.timezone("Europe/London")
 
