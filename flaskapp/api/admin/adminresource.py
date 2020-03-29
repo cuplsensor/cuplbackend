fd78b819bea1737c323b0f331d8dd3b3332ce365
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-03-29 14:48:50 +0100

    Tests all broken. Trying to fix.

--- flaskapp/api/admin/adminresource.py
+++ flaskapp/api/admin/adminresource.py
@@ -1,6 +1,7 @@
 from ..baseresource import SingleResource, MultipleResource
 from .admintokenauth import requires_admin_token
 from flask import request, jsonify
+from marshmallow import ValidationError
 
 
 class SingleAdminResource(SingleResource):
@@ -9,4 +10,20 @@ class SingleAdminResource(SingleResource):
 
 class MultipleAdminResource(MultipleResource):
     method_decorators = [requires_admin_token]
+
+    def post(self):
+        """Instantiate a model instance and return it."""
+        # Parse the data attribute as JSON.
+        jsondata = request.get_json()
+        # Create a schema for one model instance.
+        schema = self.Schema()
+        # Load schema with the JSON data
+        try:
+            schemaobj = schema.load(jsondata)
+        except ValidationError as err:
+            return err.messages, 422
+
+        schemaobj = self.service.save(schemaobj)
+        # Populate schema with the new model instance and return it.
+        return schema.dump(schemaobj)
 
