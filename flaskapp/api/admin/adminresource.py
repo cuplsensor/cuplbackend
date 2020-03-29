3fb376d1987eebac79ec0825002bfc720fad8c3c
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-03-29 15:09:12 +0100

    Most tests are working again.

--- flaskapp/api/admin/adminresource.py
+++ flaskapp/api/admin/adminresource.py
@@ -11,19 +11,5 @@ class SingleAdminResource(SingleResource):
 class MultipleAdminResource(MultipleResource):
     method_decorators = [requires_admin_token]
 
-    def post(self):
-        """Instantiate a model instance and return it."""
-        # Parse the data attribute as JSON.
-        jsondata = request.get_json()
-        # Create a schema for one model instance.
-        schema = self.Schema()
-        # Load schema with the JSON data
-        try:
-            schemaobj = schema.load(jsondata)
-        except ValidationError as err:
-            return err.messages, 422
 
-        schemaobj = self.service.save(schemaobj)
-        # Populate schema with the new model instance and return it.
-        return schema.dump(schemaobj)
 
--- flaskapp/api/baseresource.py
+++ flaskapp/api/baseresource.py
@@ -134,12 +134,17 @@ class MultipleResource(BaseResource):
 
     def post(self):
         """Instantiate a model instance and return it."""
+        # Parse the data attribute as JSON.
+        jsondata = request.get_json()
         # Create a schema for one model instance.
         schema = self.Schema()
-        # Instantiate a new model and add it to the table.
-        modelobj = self.service.create()
-        # Populate schema with the new model instance.
-        result = schema.dump(modelobj)
-        # Jsonify the dictionary.
-        return jsonify(result)
+        # Load schema with the JSON data
+        try:
+            schemaobj = schema.load(jsondata)
+        except ValidationError as err:
+            return err.messages, 422
+
+        schemaobj = self.service.save(schemaobj)
+        # Populate schema with the new model instance and return it.
+        return schema.dump(schemaobj)
 
