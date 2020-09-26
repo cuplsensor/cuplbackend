58cde582cb29b4d518c08b1e6d8e27c63437016f
Malcolm Mackay <malcolm.mackay121@gmail.com>
2020-09-26 23:15:02 +0100

    Used https://en.rakko.tools/tools/68/ to generate cupl ascii art.

--- rootapp.py
+++ rootapp.py
@@ -1,11 +1,16 @@
 from flaskapp.api.consumer.version import versioninfo
+import os
+
+f = open(os.path.join(os.path.dirname(__file__), 'logostr.txt'), 'r')
+logostr = f.read()
 
 
 def rootapp(env, resp):
     versiondict = versioninfo()
     codecversion = versiondict['cuplcodec']
     backendversion = versiondict['cuplbackend']
-    versionstr = "<a href='https://github.com/cuplsensor/cuplbackend'>cuplbackend</a> version {} running " \
+    versionstr = "<pre style='font-size:0.25em'>{}</pre>" \
+                 "<a href='https://github.com/cuplsensor/cuplbackend'>cuplbackend</a> version {} running " \
                  "<a href='https://github.com/cuplsensor/cuplcodec'>cuplcodec</a> version {}" \
                  "<ul>" \
                  "<li><a href='/docs/admin'>Admin API Docs</a></li>" \
@@ -14,6 +19,6 @@ def rootapp(env, resp):
                  "<ul>" \
                  "<li><a href='/api/admin'>Admin API Root</a></li>" \
                  "<li><a href='/api/consumer'>Consumer API Root</a></li>" \
-                 "</ul>".format(backendversion, codecversion)
+                 "</ul>".format(logostr, backendversion, codecversion)
     resp('200 OK', [('Content-Type', 'text/html')])
     return [versionstr.encode('utf-8')]
