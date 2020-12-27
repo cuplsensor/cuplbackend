7deff546d4a4f0a7b532f4f14527bef95ac427c6
Malcolm Mackay <malcolm@plotsensor.com>
2020-12-27 21:27:04 +0000

    Update rootapp.py

--- otherapps/rootapp/rootapp.py
+++ otherapps/rootapp/rootapp.py
@@ -7,6 +7,8 @@ logostr = f.read()
 from flask import Flask
 rootapp = Flask(__name__)
 
+rootapp.config["APPLICATION_ROOT"] = "/backend"
+
 @rootapp.route('/')
 def root_page():
     versiondict = versioninfo()
