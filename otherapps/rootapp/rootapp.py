d05bd1f4986a3538a8457e24c1d1fb18f500417c
Malcolm Mackay <malcolm@plotsensor.com>
2020-12-27 16:59:20 +0000

    Update rootapp.py

--- otherapps/rootapp/rootapp.py
+++ otherapps/rootapp/rootapp.py
@@ -8,5 +8,19 @@ from flask import Flask
 rootapp = Flask(__name__)
 
 @rootapp.route('/')
-def hello_world():
-    return 'Hello, World!'
+def root_page():
+    versiondict = versioninfo()
+    codecversion = versiondict['cuplcodec']
+    backendversion = versiondict['cuplbackend']
+    versionstr = "<pre style='font-size:0.25em'>{}</pre>" \
+                 "<a href='https://github.com/cuplsensor/cuplbackend'>cuplbackend</a> version {} running " \
+                 "<a href='https://github.com/cuplsensor/cuplcodec'>cuplcodec</a> version {}" \
+                 "<ul>" \
+                 "<li><a href='/docs/admin'>Admin API Docs</a></li>" \
+                 "<li><a href='/docs/consumer'>Consumer API Docs</a></li>" \
+                 "</ul>" \
+                 "<ul>" \
+                 "<li><a href='api/admin'>Admin API Root</a></li>" \
+                 "<li><a href='api/consumer'>Consumer API Root</a></li>" \
+                 "</ul>".format(logostr, backendversion, codecversion)
+   return logostr + "\n" + versionstr
