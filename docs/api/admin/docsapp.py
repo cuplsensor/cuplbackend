51f6c969aca90a5aca95739060ea3f8face388d6
Malcolm Mackay <malcolm@plotsensor.com>
2020-12-27 17:49:59 +0000

    Update docsapp.py

--- docs/api/admin/docsapp.py
+++ docs/api/admin/docsapp.py
@@ -1,6 +1,10 @@
 from flask import Flask, send_from_directory
 docsapp = Flask(__name__)
 
+@docsapp.route('/api.yaml')
+def apiyaml():
+    return send_from_directory('.', 'api.yaml')
+
 @docsapp.route('/')
 def index():
     return send_from_directory('.', 'index.html')
