03f05789b8f91c9a6709cdad0d6d629ab68b66e6
Malcolm Mackay <malcolm@plotsensor.com>
2020-12-27 16:25:17 +0000

    Update rootapp.py

--- otherapps/rootapp/rootapp.py
+++ otherapps/rootapp/rootapp.py
@@ -4,21 +4,9 @@ import os
 f = open(os.path.join(os.path.dirname(__file__), 'logostr.txt'), 'r')
 logostr = f.read()
 
+from flask import Flask
+app = Flask(__name__)
 
-def rootapp(env, resp):
-    versiondict = versioninfo()
-    codecversion = versiondict['cuplcodec']
-    backendversion = versiondict['cuplbackend']
-    versionstr = "<pre style='font-size:0.25em'>{}</pre>" \
-                 "<a href='https://github.com/cuplsensor/cuplbackend'>cuplbackend</a> version {} running " \
-                 "<a href='https://github.com/cuplsensor/cuplcodec'>cuplcodec</a> version {}" \
-                 "<ul>" \
-                 "<li><a href='/docs/admin'>Admin API Docs</a></li>" \
-                 "<li><a href='/docs/consumer'>Consumer API Docs</a></li>" \
-                 "</ul>" \
-                 "<ul>" \
-                 "<li><a href='api/admin'>Admin API Root</a></li>" \
-                 "<li><a href='api/consumer'>Consumer API Root</a></li>" \
-                 "</ul>".format(logostr, backendversion, codecversion)
-    resp('200 OK', [('Content-Type', 'text/html')])
-    return [versionstr.encode('utf-8')]
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
